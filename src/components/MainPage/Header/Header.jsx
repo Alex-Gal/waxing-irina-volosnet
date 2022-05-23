794af9a173994206f8564a53d8dd025300967976
Alexey Galashko <82465083+Alex-Gal@users.noreply.github.com>
2022-05-23 16:36:40 +0300

    Create header block

--- src/components/MainPage/Header/Header.jsx
+++ src/components/MainPage/Header/Header.jsx
@@ -1,17 +1,21 @@
 import React from 'react'
+import './Header.scss'
 
 export const Header = () => {
     return (
-        <div className="header">
-            <nav className="header__nav">
-                <ul className='nav__menu'>
-                    <li><a className='menu__link' href='#about'>Услуги</a></li>
-                    <li><a className='menu__link' href='#education'>Обучение</a></li>
-                    <li><a className='menu__link' href='#contacts'>Контакты</a></li>
-                </ul>
-            </nav>
-            <div>
-                <button onClick={() => window.open('https://dikidi.net/448067', '_blank')} className='header__button'>Записаться онлайн</button>
+        <div className='header'>
+            <div className='container'>
+                <nav className='header__nav'>
+                    <ul className='nav__menu'>
+                        <li><a className='menu__link' href='#about'>Услуги</a></li>
+                        <li><a className='menu__link' href='#education'>Обучение</a></li>
+                        <li><a className='menu__link' href='#contacts'>Контакты</a></li>
+                    </ul>
+                    <div>
+                        <button onClick={() => window.open('https://dikidi.net/448067', '_blank')} className='header__button'>Записаться онлайн</button>
+                    </div>
+                </nav>
+
             </div>
         </div>
     )
